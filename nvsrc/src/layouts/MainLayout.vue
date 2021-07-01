f85b2354a4a2867b061aa29a2cd27a216269e010
Alan Xu <xkevas@qq.com>
2021-07-01 16:55:41 +0800

    new pages

--- nvsrc/src/layouts/MainLayout.vue
+++ nvsrc/src/layouts/MainLayout.vue
@@ -1,118 +1,76 @@
+<style>
+.rowico{
+  padding-bottom: 15px;
+}
+</style>
 <template>
-  <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
+  <q-layout view="hHh lpR lfr">
+    <q-header style="height: 60px">
       <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
+        <q-icon name="explore" size="40px" color="accent"></q-icon>
 
         <q-toolbar-title>
-          Quasar App
+          Navigator
         </q-toolbar-title>
 
         <div>Quasar v{{ $q.version }}</div>
       </q-toolbar>
     </q-header>
 
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-      class="bg-grey-1"
-    >
-      <q-list>
-        <q-item-label
-          header
-          class="text-grey-8"
-        >
-          Essential Links
-        </q-item-label>
-
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
-    </q-drawer>
-
     <q-page-container>
       <router-view />
     </q-page-container>
+
+    <q-footer style="height: 300px">
+      <div class="row" style="padding-top: 20px">
+        <div class="col-md-2"></div>
+        <div class="col-md-8">
+          <div class="row">
+            <div class="col-md-4">
+              <div class="row rowico">
+                <q-img src="apache_incubator_logo.png" width="200px"></q-img>
+              </div>
+              <div class="row rowico">
+                <q-img src="ynu.png" width="200px"></q-img>
+              </div>
+              <div class="row rowico">
+                <q-img src="mxnet_logo.png" width="140px"></q-img>
+              </div>
+              <div class="row rowico">
+                <q-img src="quasar.png" width="200px"></q-img>
+              </div>
+
+
+
+            </div>
+            <div class="col-md-8">
+
+              <p>Since the Navigator Fframework uses Apache MXNet as the kernel, this framework can be used as a community-optimized version of MXNet.</p>
+
+              <p>"Copyright © 2021, Ownership of the unique design in the Navigator Framework is owned by author -  Alan Hsu and Yunnan University."</p>
+
+              <p>Apache MXNet is an effort undergoing incubation at The Apache Software Foundation (ASF), sponsored by the Apache Incubator. Incubation is required of all newly accepted projects until a further review indicates that the infrastructure, communications, and decision making process have stabilized in a manner consistent with other successful ASF projects. While incubation status is not necessarily a reflection of the completeness or stability of the code, it does indicate that the project has yet to be fully endorsed by the ASF.</p>
+
+              <p>"Copyright © 2017-2018, The Apache Software Foundation Apache MXNet, MXNet, Apache, the Apache feather, and the Apache MXNet project logo are either registered trademarks or trademarks of the Apache Software Foundation."</p>
+
+            </div>
+          </div>
+        </div>
+        <div class="col-md-2"></div>
+      </div>
+    </q-footer>
+
   </q-layout>
 </template>
 
 <script>
-import EssentialLink from 'components/EssentialLink.vue'
-
-const linksList = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-];
-
 import { defineComponent, ref } from 'vue'
 
 export default defineComponent({
   name: 'MainLayout',
-
-  components: {
-    EssentialLink
-  },
-
   setup () {
-    const leftDrawerOpen = ref(false)
-
     return {
-      essentialLinks: linksList,
-      leftDrawerOpen,
-      toggleLeftDrawer () {
-        leftDrawerOpen.value = !leftDrawerOpen.value
-      }
+
     }
   }
 })
--- nvsrc/src/pages/Index.vue
+++ nvsrc/src/pages/Index.vue
@@ -1,17 +1,177 @@
+<style lang="sass" scoped>
+.custom-caption
+  text-align: center
+  padding: 12px
+  color: white
+.force-22
+  height: 22px!important
+.selection
+  background-color: #E1DDE0
+  margin-right: 5px
+  cursor: pointer
+  color: #616161 !important
+.selection-active
+  background-color: #66229B
+  margin-right: 5px
+  cursor: pointer
+  color: white
+</style>
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+  <q-page class="flex">
+    <div style="width: 100%">
+      <q-carousel
+        arrows
+        animated
+        v-model="slide"
+        height="450px"
+      >
+        <q-carousel-slide name="first" img-src="banner.jpg">
+          <div class="absolute-center custom-caption">
+            <div class="text-h2">Navigator Framework</div>
+            <div class="text-subtitle1">A deep learning framework that encourages people to share.</div>
+            <div style="padding-top: 30px">
+              <q-btn color="white" text-color="black" label="立刻安装" style="display: none" />
+            </div>
+          </div>
+          <div class="absolute-bottom-right custom-caption">
+            <div class="text-subtitle1">最新版本：0.1.20   发布日期：2021年6月30日</div>
+          </div>
+        </q-carousel-slide>
+      </q-carousel>
+    </div>
+    <div id="install" style="width: 100%;margin-top: 50px">
+      <div class="row">
+        <div class="col-md-2"></div>
+        <div class="col-md-8">
+          <div class="text-h5">安装 Navigator Framework</div>
+          <div class="text-subtitle1" style="margin: 20px 0 20px 0;">请在下方选择您的首选配置，以获得具体的安装方式或命令。</div>
+          <div class="row">
+            <div class="col-md-1">
+              <div class="col-md-12">
+                <p class="text-subtitle2 text-grey-6">|内核引擎</p>
+              </div>
+              <div class="col-md-12">
+                <p class="text-subtitle2 text-grey-6">|操作系统</p>
+              </div>
+              <div class="col-md-12">
+                <p class="text-subtitle2 text-grey-6">|开发语言</p>
+              </div>
+              <div class="col-md-12">
+                <p class="text-subtitle2 text-grey-6">|计算平台</p>
+              </div>
+              <div class="col-md-12">
+                <p class="text-subtitle2 text-grey-6">|版本</p>
+              </div>
+              <div class="col-md-12">
+                <p class="text-subtitle2 text-grey-6">|安装命令</p>
+              </div>
+            </div>
+            <div class="col-md-3">
+              <div class="col-md-12">
+                <div style="background-color: #66229B;width: 500px">
+                  <p class="text-subtitle2 selection-active" style="text-align: center">MXNet 1.7.0</p>
+                </div>
+              </div>
+              <div class="col-md-12 row" style="width: 500px;text-align: center;">
+                <div class="col force-22 selection-active platform" id="Windows" @click="switch_platform('Windows')">
+                  <p class="text-subtitle2">Windows</p>
+                </div>
+                <div class="col force-22 selection platform" id="Linux" @click="switch_platform('Linux')">
+                  <p class="text-subtitle2">Linux</p>
+                </div>
+                <div class="col force-22 selection platform" id="MacOS" @click="switch_platform('MacOS')">
+                  <p class="text-subtitle2">MacOS</p>
+                </div>
+                <div class="col force-22 selection platform" id="Cloud" @click="switch_platform('Cloud')">
+                  <p class="text-subtitle2">Cloud</p>
+                </div>
+                <div class="col force-22 selection platform" id="Raspberry" @click="switch_platform('Raspberry')">
+                  <p class="text-subtitle2">Raspberry</p>
+                </div>
+
+              </div>
+
+              <div class="col-md-12" style="margin-top: 16px">
+                <div style="background-color: #66229B;width: 500px">
+                  <p class="text-subtitle2 selection-active" style="text-align: center">Python</p>
+                </div>
+              </div>
+
+              <div class="row col-md-12" style="width: 500px;text-align: center;">
+                <div class="col force-22 selection-active cuda" @click="switch_cuda('cu102')">
+                  <p class="text-subtitle2" id="cu102">CUDA 10.2</p>
+                </div>
+                <div class="col force-22 selection cuda" @click="switch_cuda('cu111')">
+                  <p class="text-subtitle2" id="cu111">CUDA 11.1</p>
+                </div>
+                <div class="col force-22 selection cuda" @click="switch_cuda('cpu')">
+                  <p class="text-subtitle2" id="cpu">CPU Only</p>
+                </div>
+              </div>
+
+              <div class="col-md-12" style="margin-top: 16px">
+                <div style="background-color: #66229B;width: 500px">
+                  <p class="text-subtitle2 selection-active" style="text-align: center">0.1.20</p>
+                </div>
+              </div>
+              <div class="col-md-12" style="margin-top: 16px;width: 500px">
+                <pre style="background-color: #e0e0e0">
+
+  # 方式一：通过nwpm安装（推荐）
+  pip3 install nwpm=={{version}}
+  nwpm -create {{engine}}_{{cuda}} -platform {{platform}} -version 0.1.20
+
+  # 方式二：直接通过pip安装
+  pip3 install nvmx{{cuda}}=={{version}}
+                </pre>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-2"></div>
+      </div>
+
+    </div>
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue';
+import { ref } from 'vue'
 
-export default defineComponent({
-  name: 'PageIndex'
-})
+export default {
+  data(){
+    return{
+      engine: 'mx170',
+      platform: 'Windows',
+      language: 'Python',
+      cuda: 'cu102',
+      version: '0.1.20'
+    }
+  },
+  setup () {
+    return {
+      slide: ref('first'),
+    }
+  },
+  methods:{
+    switch_platform(keyword){
+      var plat = document.getElementsByClassName('platform')
+      var platlen = document.getElementsByClassName('platform').length
+      for(var i=0; i<platlen; i++){
+        plat[i].classList = "col force-22 selection platform"
+      }
+      document.getElementById(keyword).classList = "col force-22 selection-active platform"
+      this.platform = keyword
+    },
+    switch_cuda(keyword){
+      var plat = document.getElementsByClassName('cuda')
+      var platlen = document.getElementsByClassName('cuda').length
+      for(var i=0; i<platlen; i++){
+        plat[i].classList = "col force-22 selection cuda"
+      }
+      document.getElementById(keyword).classList = "col force-22 selection-active cuda"
+      this.cuda = keyword
+    }
+  }
+}
 </script>
